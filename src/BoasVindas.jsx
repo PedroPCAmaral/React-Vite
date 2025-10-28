32f73c2e7d6a37ca27fc9147e63d6076ece75638
Pedro Amaral <pedropaulocostadoamaral@gmail.com>
2025-10-28 17:24:26 -0300

    Atividade 1

---
+++ src/BoasVindas.jsx
@@ -0,0 +1,10 @@
+function BoasVindas() {
+  return (
+    <div>
+      <h2>Seja bem-vindo(a) ao React!</h2>
+      <p>Hoje estamos aprendendo os fundamentos dessa poderosa biblioteca</p>
+    </div>
+  );
+}
+
+export default BoasVindas;
