35339e3c5dfd3c60e8262de198079dd31a2fbc0d
Pedro Amaral <pedropaulocostadoamaral@gmail.com>
2025-10-31 14:39:30 -0300

    React + Vite + Bootstrap

---
+++ projeto7/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Bootstrap Funcionando 🎉</h1>
+      <p className="lead">Agora você pode usar as classes do Bootstrap!</p>
+      <button className="btn btn-success mt-3">Clique Aqui</button>
+    </div>
+  );
+}
+
+export default App;
